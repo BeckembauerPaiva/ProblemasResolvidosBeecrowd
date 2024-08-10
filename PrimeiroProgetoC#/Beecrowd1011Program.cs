a8e96c7cbcfe4c7341eba55d86911be774007fa6
BeckembauerPaiva <168041127+BeckembauerPaiva@users.noreply.github.com>
2024-08-10 13:04:12 -0300

    Add files via upload

---
+++ PrimeiroProgetoC#/Beecrowd1011Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicios
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            double raio, pi,volume;
+            raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            pi = 3.14159;
+            volume = (4.0/3.0) * pi * (raio * raio * raio);
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+            
+        }
+    }
+}
